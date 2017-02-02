f0cee61a3343121d26bd21c79da6f634b3eb5928
Rafael Vieira <rz.vieira@outlook.com>
2017-02-02 17:14:04 -0200

    commit initial structure login

---
+++ app/app.module.js
@@ -0,0 +1,37 @@
+"use strict";
+//import { AppComponent }  from './app.component';
+//import { routing }        from './app.routing';
+// import { AlertComponent } from './_directives/index';
+// import { AuthGuard } from './_guards/index';
+// import { AlertService, AuthenticationService, UserService } from './_services/index';
+// import { HomeComponent } from './home/index';
+// import { LoginComponent } from './login/index';
+// import { RegisterComponent } from './register/index';
+// @NgModule({
+//     imports: [
+//         BrowserModule,
+//         FormsModule,
+//         HttpModule,
+//         routing
+//     ],
+//     declarations: [
+//         AppComponent,
+//         AlertComponent,
+//         HomeComponent,
+//         LoginComponent,
+//         RegisterComponent
+//     ],
+//     providers: [
+//         AuthGuard,
+//         AlertService,
+//         AuthenticationService,
+//         UserService,
+//         // providers used to create fake backend
+//         fakeBackendProvider,
+//         MockBackend,
+//         BaseRequestOptions
+//     ],
+//     bootstrap: [AppComponent]
+// })
+// export class AppModule { } 
+//# sourceMappingURL=app.module.js.map
